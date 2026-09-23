fea86706631db747809bc838097b1fcae3bd8a6e
nipul830 <57082785+nipul830@users.noreply.github.com>
2026-09-23 12:31:36 +0530

    Add native screen-share plugin activity

---
+++ android-overrides/app/src/main/java/com/courseera/app/MainActivity.java
@@ -0,0 +1,12 @@
+package com.courseera.app;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(ScreenSharePlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
