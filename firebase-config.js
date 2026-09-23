0b93b33b459fe0e3ced4e0ae4d7d5415a6e09366
nipul830 <57082785+nipul830@users.noreply.github.com>
2026-09-23 07:58:52 +0530

    Add Firebase client configuration

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Course Era Firebase client configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqrMvKivSvrVEr8hwDpVWg8f3zFfZttLVQ",
+  authDomain: "courseera-22425.firebaseapp.com",
+  projectId: "courseera-22425",
+  storageBucket: "courseera-22425.firebasestorage.app",
+  messagingSenderId: "263442433438",
+  appId: "1:263442433438:web:31a490f6ee3579322f828f",
+  measurementId: "G-3T6QSQNN2M"
+};
+
+if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
+const ceAuth = firebase.auth();
+ceAuth.setPersistence(firebase.auth.Auth.Persistence.LOCAL);
